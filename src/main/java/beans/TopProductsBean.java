c0afb0951c34467738e94ebb64af71bdd6746800
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2018-10-30 13:49:19 +0100

    created beans to display top products

---
+++ src/main/java/beans/TopProductsBean.java
@@ -0,0 +1,45 @@
+package beans;
+
+import dao.Dao;
+import model.Product;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ApplicationScoped
+public class TopProductsBean {
+    private Product choiceProduct;
+    private List<SelectItem> topProducts;
+    private Dao dao = Dao.getDao();
+
+    public TopProductsBean(){
+        this.topProducts = new ArrayList<>();
+        List<Product> products = dao.getTopProducts();
+        for (Product p : products)
+            this.topProducts.add(new SelectItem(p, p.getName()));
+    }
+
+
+    public Product getChoiceProduct() {
+        return choiceProduct;
+    }
+
+    public void setChoiceProduct(Product choiceProduct) {
+        this.choiceProduct = choiceProduct;
+    }
+
+    public List<SelectItem> getTopProducts() {
+        return topProducts;
+    }
+
+    public void setTopProducts(List<SelectItem> topProducts) {
+        this.topProducts = topProducts;
+    }
+
+
+
+}
