d21ee4b6acb51a4d257925bbf0caf40c47155439
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2018-10-30 10:55:55 +0100

    created list of products

--- src/main/java/beans/CategoryBean.java
+++ src/main/java/beans/CategoryBean.java
@@ -1,4 +1,26 @@
 package beans;
 
+import dao.Dao;
+import model.Category;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.ListDataModel;
+
+@ManagedBean
+@RequestScoped
 public class CategoryBean {
+    private ListDataModel<Category> categoryModel = new ListDataModel<>();
+
+    public ListDataModel<Category> getCategoryModel() {
+        return categoryModel;
+    }
+
+    public void setCategoryModel(ListDataModel<Category> categoryModel) {
+        this.categoryModel = categoryModel;
+    }
+
+    public CategoryBean() {
+        categoryModel.setWrappedData(Dao.getDao().getCategoryList());
+    }
 }
---
+++ src/main/java/beans/ProductListBean.java
@@ -0,0 +1,56 @@
+package beans;
+
+import dao.Dao;
+import model.Category;
+import model.Product;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.ListDataModel;
+import java.util.List;
+
+@ManagedBean
+@SessionScoped
+public class ProductListBean {
+    private ListDataModel<Product> productModel = new ListDataModel<>();
+    private Category category;
+    private int page = 0;
+
+    private Dao dao = Dao.getDao();
+
+    public ProductListBean(){
+        this.refreshModel();
+    }
+
+    private void refreshModel() {
+        List<Product> list = dao.getProductByCategory(category,page * Dao.PAGE_SIZE, Dao.PAGE_SIZE);
+        this.productModel.setWrappedData(list);
+    }
+
+    public ListDataModel<Product> getProductModel() {
+        return productModel;
+    }
+
+    public void setProductModel(ListDataModel<Product> productModel) {
+        this.productModel = productModel;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean isNext(){
+        double maxPage = Math.ceil((dao.getCountProducts(this.category)+0.0) / Dao.PAGE_SIZE) - 1;
+        return (page < maxPage);
+    }
+
+    public boolean isPrevious(){
+        return this.page > 0;
+    }
+
+
+}
